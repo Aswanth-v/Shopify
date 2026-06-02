9df6e52944151f7be50484fdf773688d9303ce1b
Aswabnth.v <aswanthv695@gmail.com>
2026-06-02 04:05:07 -0700

    data maping is successfull

--- product-dashboard/app/page.tsx
+++ product-dashboard/app/page.tsx
@@ -1,25 +1,37 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Page as PolarisPage } from "@shopify/polaris";
+import ProductTable from "@/components/ProductTable";
+import ProductModal from "@/components/ProductModel";
 import type { Product } from "@/types/Product";
 
 export default function Page() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [selectedProduct, setSelectedProduct] =
+    useState<Product | null>(null);
 
-   useEffect(() => {
-    console.log("Fetching data...");
-
+  useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
-      .then((data) => {
-        console.log("Data:", data);
+      .then((data)=>{
         setProducts(data);
-      });
+        console.log(data)
+      })
+      .catch((error) => console.error("Error fetching products:", error));
   }, []);
 
   return (
-    <div>
-      <h1>Products</h1>
-    </div>
+    <PolarisPage title="Products">
+      <ProductTable
+        products={products}
+        onSelect={setSelectedProduct}
+      />
+
+      <ProductModal
+        product={selectedProduct}
+        onClose={() => setSelectedProduct(null)}
+      />
+    </PolarisPage>
   );
 }
