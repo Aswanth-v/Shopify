a8a7555a896b8e7ec5861e8a31bb461de09cc925
Aswabnth.v <aswanthv695@gmail.com>
2026-06-02 03:42:29 -0700

    data fetching is working

--- product-dashboard/app/page.tsx
+++ product-dashboard/app/page.tsx
@@ -1,15 +1,25 @@
 "use client";
 
-import { Page, Card, Text } from "@shopify/polaris";
+import React, { useState, useEffect } from "react";
+import type { Product } from "@/types/Product";
 
-export default function Home() {
+export default function Page() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+   useEffect(() => {
+    console.log("Fetching data...");
+
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Data:", data);
+        setProducts(data);
+      });
+  }, []);
+
   return (
-    <Page title="Products">
-      <Card>
-        <Text as="h2" variant="headingMd">
-          Shopify Polaris is working 🚀
-        </Text>
-      </Card>
-    </Page>
+    <div>
+      <h1>Products</h1>
+    </div>
   );
 }
